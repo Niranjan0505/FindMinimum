fd4f5b64129e3b6885569b2f252952a65de024b5
Niranjan0505 <ezhil.niranjan@gmail.com>
2025-01-07 17:35:05 -0500

    add three file

--- src/FindMinimum.java
+++ src/FindMinimum.java
@@ -1,5 +1,12 @@
-public class Main {
+public class FindMinimum {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[] numbers={2,6,11,20,3,7,18};
+        int min=numbers[0];
+        for (int i=1; i < numbers.length; i++)
+            if (numbers[i]<min){
+                min=numbers[i];
+            }
+
+        System.out.println("The minimum value :" +min);
     }
 }
