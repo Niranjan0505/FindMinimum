b2bc5d81d12415d8c5bd25f0fd433adfccd0bd76
Niranjan0505 <ezhil.niranjan@gmail.com>
2025-01-20 19:55:21 -0500

    add findArrayReverse and findElementInArray and min and max

--- src/findArrayReverse.java
+++ src/findArrayReverse.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class findArrayReverse {
     public static void main(String[] args) {
         int[] array={10,20,30,40,50,60,70};
@@ -10,5 +12,33 @@ public class findArrayReverse {
             System.out.println("Index is not bound");
         }
 
+    }
+
+    public static class StringInInput {
+        public static void main(String[] args) {
+            Scanner scanner= new Scanner(System.in);
+            String[] inputs =new String[3];
+            String target="a";
+            System.out.println("Enter three string:");
+        for (int i = 0; i < 3; i++ ){
+                System.out.println("Input"+(i+1)+":");
+                inputs[i]= scanner.nextLine();
+            }
+            boolean found=false;
+        for (String input:inputs){
+                if (input.equals(target)){
+                    found=true;
+                    break;
+                }
+            }
+
+        if(found) {
+                System.out.println("The target string "+target+" was found");
+            }else{
+                System.out.println("The target string " +target+"  not found");
+            }
+            scanner.close();
+
+        }
     }
 }
